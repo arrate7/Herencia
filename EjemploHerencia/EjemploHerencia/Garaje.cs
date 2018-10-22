453a547f0f969740c2b620af9fa45b09db271f44
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-10-22 12:50:43 +0200

    .

---
+++ EjemploHerencia/EjemploHerencia/Garaje.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploHerencia
+{
+    class Garaje
+    {
+        private string nombre;
+        private List<CocheSimple> garaje;
+        private int numeroCoches;
+
+        public Garaje(string nombre)
+        {
+            this.nombre = nombre;
+            garaje = new List<CocheSimple>();
+        }
+        public Garaje(string nombre, List<CocheSimple> garaje)
+        {
+            this.nombre = nombre;
+            this.garaje = garaje;
+        }
+
+        public void AñadirCoche(CocheSimple c)
+        {
+            garaje.Add(c);
+        }
+
+        public List<CocheSimple> GetGaraje()
+        {
+            return garaje;
+        }
+    }
+}
--- EjemploHerencia/EjemploHerencia/Program.cs
+++ EjemploHerencia/EjemploHerencia/Program.cs
@@ -16,19 +16,24 @@ namespace EjemploHerencia
             Autobus a1 = new Autobus("Alsa", "azul",100,"ztz");
             CocheSimple c1 = new CocheSimple("Ferrari", "Testa Rosa", 180);
 
-            List<CocheSimple> listaCoches = new List<CocheSimple>()
+            List<CocheSimple> list1 = new List<CocheSimple>()
             {
                 t1,a1,c1
             };
-            
-            foreach(CocheSimple coche in listaCoches)
+
+            Garaje g1 = new Garaje("Garaje de Arrate",list1);
+            //g1.AñadirCoche(t1);
+            //g1.AñadirCoche(a1);
+            //g1.AñadirCoche(c1);
+
+            foreach (CocheSimple coche in g1.GetGaraje())
             {
                 Console.WriteLine(coche.GetType().Name.ToUpper());
-                Console.WriteLine(coche.MostrarDatos());
+                Console.WriteLine(coche.ToString());
                 Console.WriteLine("**************************");
             }
 
-            Console.WriteLine("Número de Vehículos: " + listaCoches.Count);
+            Console.WriteLine("Número de Vehículos: " + g1.GetGaraje().Count);
             Console.Read();
         }
     }

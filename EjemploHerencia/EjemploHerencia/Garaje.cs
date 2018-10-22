a6a3986ff7b0f4d33bec44cb4449de5dfdf75736
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-10-22 13:32:04 +0200

    .

--- EjemploHerencia/EjemploHerencia/Garaje.cs
+++ EjemploHerencia/EjemploHerencia/Garaje.cs
@@ -11,7 +11,7 @@ namespace EjemploHerencia
     {
         private string nombre;
         private List<CocheSimple> garaje;
-        private int numeroCoches;
+        private int numeroTaxis, numeroAutobuses;
 
         //Este constructor inicia una lista vacia
         public Garaje(string nombre)
@@ -28,13 +28,45 @@ namespace EjemploHerencia
         //Este metodo recibe un objeto del tipo CocheSimple(puede ser: taxi, autobus o cochesimple) y lo añade a la lista
         public void AñadirCoche(CocheSimple c)
         {
-            garaje.Add(c);
+            if (MeterVehiculo(c))
+            {
+                if (c.GetType().Name == "Taxi")
+                {
+                    numeroTaxis++;
+                }
+                else if(c.GetType().Name == "Autobus")
+                {
+                    numeroAutobuses++;
+                }
+                garaje.Add(c);
+            }
         }
 
         //Es un getter para que te devuelva la lista de CocheSimple
         public List<CocheSimple> GetGaraje()
         {
             return garaje;
+        }
+
+        public Boolean MeterVehiculo(CocheSimple c)
+        {
+            if (garaje.Count < 6)
+            {
+                if (c.GetType().Name == "Autobus" && numeroAutobuses < 1)
+                {
+                    return true;
+                }
+                else if (c.GetType().Name == "Taxi" && numeroTaxis < 1)
+                {
+                    return true;
+                }
+                else if (c.GetType().Name == "CocheSimple")
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
